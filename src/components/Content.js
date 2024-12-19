2c938f9a44c22533bb62331fa0d26177c3429d19
sairamkiran9 <sairamkiranmuppana.1999@gmail.com>
2024-12-19 16:32:56 -0500

    project template

--- src/components/Content.js
+++ src/components/Content.js
@@ -2,12 +2,14 @@ import React from "react";
 import Profile from "./Profile";
 import About from "./About";
 import More from "./More";
+import Projects from "./Projects";
 
 const Content = () => {
     return (
         <body className="container-body">
             <Profile />
             <About />
+            <Projects />
             <More />
         </body>
     );
